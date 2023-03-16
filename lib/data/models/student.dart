9712301e505e257e287b0d986dd4b26e224a93c9
sajeewan <sajeewan01@gmail.com>
2023-03-16 23:04:11 +0530

    Student, Empoyee List Added

---
+++ lib/data/models/student.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Student {
+  final String name;
+  final String email;
+  final String image;
+  final String clz;
+  final String phoneNo;
+  final String dob;
+
+  Student({
+    required this.name,
+    required this.email,
+    required this.image,
+    required this.clz,
+    required this.dob,
+    required this.phoneNo,
+  });
+
+  static Student fromDocumentSnapshot(DocumentSnapshot snapshot) {
+    Map<String, dynamic> data = snapshot.data()! as Map<String, dynamic>;
+    return Student(
+      name: data['name'],
+      email: data['email'],
+      image: data['image'],
+      clz: data['class'],
+      dob: data['dob'],
+      phoneNo: data['phoneNo'],
+    );
+  }
+}
