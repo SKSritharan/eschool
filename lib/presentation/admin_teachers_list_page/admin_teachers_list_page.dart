068ffcbba7eaf1a36ae923fb0746528b988f1131
SKSritharan <sritharansk@coderay.lk>
2023-03-17 12:07:25 +0530

    update bottom modal sheet

--- lib/presentation/admin_teachers_list_page/admin_teachers_list_page.dart
+++ lib/presentation/admin_teachers_list_page/admin_teachers_list_page.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:eschool/widgets/custom_imagepicker.dart';
+
 import '../../data/models/teacher.dart';
 import '../admin_teachers_list_page/widgets/listrectangleone4_item_widget.dart';
 import 'controller/admin_teachers_list_controller.dart';
@@ -85,7 +89,8 @@ class AdminTeachersListPage extends StatelessWidget {
                                 teacher.subject,
                                 textAlign: TextAlign.left,
                               ),
-                              onTap: () {},
+                              onTap: () => _showTeacherModalBottomSheet(
+                                  context, teacher.id),
                             ),
                           ),
                         ));
@@ -105,5 +110,60 @@ class AdminTeachersListPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _showTeacherModalBottomSheet(BuildContext context, userId) {
+    showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        builder: (BuildContext context) {
+          return SingleChildScrollView(
+            physics: ClampingScrollPhysics(),
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    'Edit Teacher',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  CustomImagePicker(
+                    size: 50,
+                    onImageSelected: (File file) {},
+                    currentImage:
+                        "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80",
+                  ),
+                  TextFormField(
+                    controller: null,
+                    decoration: InputDecoration(labelText: 'Name'),
+                  ),
+                  SizedBox(height: 16.0),
+                  TextFormField(
+                    controller: null,
+                    decoration: InputDecoration(labelText: 'Email'),
+                    enabled: false,
+                  ),
+                  SizedBox(height: 16.0),
+                  TextFormField(
+                    controller: null,
+                    decoration: InputDecoration(labelText: 'Subject'),
+                  ),
+                  SizedBox(height: 16.0),
+                  ElevatedButton(
+                    child: Text('Save'),
+                    onPressed: () {},
+                  ),
+                  SizedBox(height: MediaQuery.of(context).viewInsets.bottom),
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
